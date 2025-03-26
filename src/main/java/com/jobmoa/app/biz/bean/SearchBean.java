425d39caa57605e4a149616f831a14b4df933629
SD <66343468+no1fc@users.noreply.github.com>
2025-03-26 16:06:30 +0900

    선택 기능 및 UI/로직 개선 업데이트
    - `participantMain.jsp`:
      - 선택된 참여자 이름 불러오는 `getParticipantName` 함수 추가.
      - 삭제/마감 버튼 클릭 시 확인창(SweetAlert)과 연동하여 사용자 확인 로직 추가.
      - 삭제 요청 로직 및 마감 상태 변경 로직 개선.
    
    - `SearchBean.java`:
      - `toString` 메서드에서 null 및 빈값 체크 로직 개선.
      - 기본값 설정 변경(`endDateOption`, `searchOption` 등).
    
    - `Report-mapping.xml`:
      - 취소 상태의 참여자를 제외시키는 조건 추가.
    
    - `Participant-mapping.xml`:
      - `ORDER BY` 관련 불필요한 조건 제거로 효율성 개선.
    
    - `DailyWorkReportPage.jsp`:
      - 금일/금주/금월/금년 누적 실적 및 금일 배정 인원 UI 추가.
      - 전담자 목록 상의 드래그 핸들 및 체크박스 UI 개선.
    
    - `sweetAlert.js`:
      - SweetAlert에서 버튼 순서 설정 변경(`reverseButtons: false`).
    
    기능 및 UI 전반적인 개선을 통해 사용자 경험 향상 및 코드 가독성 개선.

--- src/main/java/com/jobmoa/app/biz/bean/SearchBean.java
+++ src/main/java/com/jobmoa/app/biz/bean/SearchBean.java
@@ -20,15 +20,15 @@ public class SearchBean {
     @Override
     public String toString() {
         //SearchBean 으로 들어온 값이 null or 0 이면 값을 제거 후 등록
-        this.search = (this.search == null)?"":this.search;
-        this.endDateOption = (this.endDateOption == null)?"":this.endDateOption;
-        this.searchOption = (this.searchOption == null)?"":this.searchOption;
-        this.searchType = (this.searchType == null)?"":this.searchType;
+        this.search = (this.search == null || this.search.isEmpty())?"":this.search;
+        this.endDateOption = (this.endDateOption == null || this.endDateOption.isEmpty())?"allType":this.endDateOption;
+        this.searchOption = (this.searchOption == null || this.searchOption.isEmpty())?"참여자":this.searchOption;
+        this.searchType = (this.searchType == null || this.searchType.isEmpty())?"":this.searchType;
         int pageString = (this.page == 0)?1:this.page;
         int pageRowsString = (this.pageRows == 0)?100:this.pageRows;
-        this.column = (this.column == null)?"":this.column;
-        this.order = (this.order == null)?"":this.order;
-        this.participantRegDate = (this.participantRegDate == null)?"All":this.participantRegDate;
+        this.column = (this.column == null || this.column.isEmpty())?"":this.column;
+        this.order = (this.order == null || this.order.isEmpty())?"":this.order;
+        this.participantRegDate = (this.participantRegDate == null || this.participantRegDate.isEmpty())?"All":this.participantRegDate;
 
         String returnValue ="page=" + pageString +
                 "&endDateOption=" + endDateOption +
